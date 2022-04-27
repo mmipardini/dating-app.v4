0399bf6fae250dfb1d716dde8f9cc57f9ef14b4e
mmipardini <micaela.mqs@gmail.com>
2022-04-27 00:03:21 -0300

    get users and get user by id

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUserById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
