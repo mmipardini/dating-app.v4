5c8322537ae73db905dec21d0bda82e822b86a38
mmipardini <micaela.mqs@gmail.com>
2022-04-27 00:15:26 -0300

    cors etc

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -46,6 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
